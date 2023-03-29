2ab4c66ec184c971bd7ddd3fb72b81d5a13f1f6e
NimaKhalili <khalili7091@gmail.com>
2023-03-29 17:28:56 +0430

    Update MainActivity.kt replace baseActivity

--- app/src/main/java/com/example/citiesdistance/MainActivity.kt
+++ app/src/main/java/com/example/citiesdistance/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.citiesdistance
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

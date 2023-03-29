1df11ee7a563f64e246cb6d7fd6dcba58b643e2a
NimaKhalili <khalili7091@gmail.com>
2023-03-29 17:00:17 +0430

    Add base.kt base classes and interface

--- app/src/main/java/com/example/citiesdistance/base.kt
+++ app/src/main/java/com/example/citiesdistance/base.kt
@@ -1,2 +1,11 @@
 package com.example.citiesdistance
 
+abstract class BaseFragment : BaseView
+
+abstract class BaseActivity : BaseView
+
+interface BaseView {
+    fun setProgressIndicator(mustShow: Boolean)
+}
+
+abstract class BaseViewModel

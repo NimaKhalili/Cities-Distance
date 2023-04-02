0f86dd813fec292f93961513813a7e746a76a08d
NimaKhalili <khalili7091@gmail.com>
2023-04-02 12:09:27 +0430

    Add build.gradle timber library

--- app/src/main/java/com/example/citiesdistance/App.kt
+++ app/src/main/java/com/example/citiesdistance/App.kt
@@ -6,11 +6,13 @@ import com.example.citiesdistance.services.http.createApiServiceInstance
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
+import timber.log.Timber
 
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        Timber.plant()
 
         val myModules = module {
             single<ApiService> { createApiServiceInstance() }

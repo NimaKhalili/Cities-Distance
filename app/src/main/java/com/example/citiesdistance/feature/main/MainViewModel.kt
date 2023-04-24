4b20f7da74a662f8b80daeff2fe242eca3d0d361
NimaKhalili <khalili7091@gmail.com>
2023-04-24 12:04:05 +0430

    Update MainViewModel.kt replace baseSingleObserver with singleObserver

--- app/src/main/java/com/example/citiesdistance/feature/main/MainViewModel.kt
+++ app/src/main/java/com/example/citiesdistance/feature/main/MainViewModel.kt
@@ -1,14 +1,12 @@
 package com.example.citiesdistance.feature.main
 
 import androidx.lifecycle.MutableLiveData
+import com.example.citiesdistance.common.BaseSingleObserver
 import com.example.citiesdistance.common.BaseViewModel
 import com.example.citiesdistance.data.repo.DistanceRepository
 import com.google.gson.JsonElement
-import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import timber.log.Timber
 
 class MainViewModel(private val distanceRepository: DistanceRepository) : BaseViewModel() {
     val distanceLiveData = MutableLiveData<JsonElement>()
@@ -17,19 +15,10 @@ class MainViewModel(private val distanceRepository: DistanceRepository) : BaseVi
         distanceRepository.getDistance(beginning, destination)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : SingleObserver<JsonElement> {
-                override fun onSubscribe(d: Disposable) {
-                    compositeDisposable.add(d)
-                }
-
-                override fun onError(e: Throwable) {
-                    Timber.e(e)
-                }
-
+            .subscribe(object : BaseSingleObserver<JsonElement>(compositeDisposable) {
                 override fun onSuccess(t: JsonElement) {
                     distanceLiveData.value = t
                 }
-
             })
     }
 }

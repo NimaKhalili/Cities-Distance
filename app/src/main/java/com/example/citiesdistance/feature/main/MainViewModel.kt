cd1981ef62b2591c7063f7d4c71c95146bdab59a
NimaKhalili <khalili7091@gmail.com>
2023-04-25 09:06:28 +0430

    Update MainViewModel.kt asyncNetworkRequest with subscribeOn and observeOn

--- app/src/main/java/com/example/citiesdistance/feature/main/MainViewModel.kt
+++ app/src/main/java/com/example/citiesdistance/feature/main/MainViewModel.kt
@@ -3,18 +3,16 @@ package com.example.citiesdistance.feature.main
 import androidx.lifecycle.MutableLiveData
 import com.example.citiesdistance.common.BaseSingleObserver
 import com.example.citiesdistance.common.BaseViewModel
+import com.example.citiesdistance.common.asyncNetworkRequest
 import com.example.citiesdistance.data.repo.DistanceRepository
 import com.google.gson.JsonElement
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
 class MainViewModel(private val distanceRepository: DistanceRepository) : BaseViewModel() {
     val distanceLiveData = MutableLiveData<JsonElement>()
 
     fun getDistance(beginning: String, destination: String) {
         distanceRepository.getDistance(beginning, destination)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
+            .asyncNetworkRequest()
             .subscribe(object : BaseSingleObserver<JsonElement>(compositeDisposable) {
                 override fun onSuccess(t: JsonElement) {
                     distanceLiveData.value = t

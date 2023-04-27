554df73122fa538d092fc6321b0e011db9495473
NimaKhalili <khalili7091@gmail.com>
2023-04-27 09:02:40 +0430

    Add base.kt progressBar in baseView and put it in BaseActivity and BaseFragment and organize

--- app/src/main/java/com/example/citiesdistance/common/base.kt
+++ app/src/main/java/com/example/citiesdistance/common/base.kt
@@ -1,27 +1,71 @@
 package com.example.citiesdistance.common
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import com.example.citiesdistance.R
 import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseFragment : Fragment(), BaseView {
-    override fun setProgressIndicator(mustShow: Boolean) {
-        TODO("Not yet implemented")
-    }
+    override val rootView: ConstraintLayout?
+        get() = requireView() as ConstraintLayout
+    override val viewContext: Context?
+        get() = context
 }
 
 abstract class BaseActivity : AppCompatActivity(), BaseView {
-    override fun setProgressIndicator(mustShow: Boolean) {
-        TODO("Not yet implemented")
+    override val rootView: ConstraintLayout?
+        get() = prepareView()
+
+    private fun prepareView(): ConstraintLayout {
+        val viewGroup = window.decorView.findViewById(android.R.id.content) as ViewGroup
+        if (viewGroup !is ConstraintLayout) {
+            viewGroup.children.forEach {
+                if (it is ConstraintLayout) return it
+            }
+            throw IllegalStateException("RootView must be instance of ConstraintLayout")
+        } else {
+            return viewGroup
+        }
     }
+
+    override val viewContext: Context?
+        get() = this
 }
 
 interface BaseView {
-    fun setProgressIndicator(mustShow: Boolean)
+    val rootView: ConstraintLayout?
+    val viewContext: Context?
+    fun setProgressIndicator(mustShow: Boolean) {
+        rootView?.let {
+            var loadingView = it.findViewById<View>(R.id.frameLayout_viewLoading)
+            viewContext?.let { context ->
+                loadingView = addLoadingView(loadingView, mustShow, context, it)
+                loadingView?.visibility = if (mustShow) View.VISIBLE else View.GONE
+            }
+        }
+    }
+
+    fun addLoadingView(
+        loadingView: View?, mustShow: Boolean, context: Context, it: ConstraintLayout
+    ): View? {
+        var loadingView1 = loadingView
+        if (loadingView1 == null && mustShow) {
+            loadingView1 =
+                LayoutInflater.from(context).inflate(R.layout.view_loading, it, false)
+            it.addView(loadingView1)
+        }
+        return loadingView1
+    }
 }
 
-abstract class BaseViewModel:ViewModel(){
+abstract class BaseViewModel : ViewModel() {
     val compositeDisposable = CompositeDisposable()
     override fun onCleared() {
         compositeDisposable.clear()

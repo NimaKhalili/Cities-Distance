1bfed3eed37e1dbb5efa7c48693edf495ac3f7c3
NimaKhalili <khalili7091@gmail.com>
2023-05-02 08:28:39 +0430

    Add MainActivity.kt bottomNavigationView

--- app/src/main/java/com/example/citiesdistance/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/citiesdistance/feature/main/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.citiesdistance.feature.main
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.citiesdistance.R
 import com.example.citiesdistance.common.BaseActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fragmentContainer_main
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Setup the bottom navigation view with navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView_main)
+        bottomNavigationView.setupWithNavController(navController)
+
+        // Setup the ActionBar with navController and 3 top level destinations
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home, R.id.history,  R.id.gas)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
     }
 }
